3711aaf231eb1a4b386de57c3475e1e21cbf331c
bishlrajbanshi <bishalrajbanshi.in@gmail.com>
2024-10-08 20:13:54 +0545

    day10

--- JsFunctions/JsFunc1.js
+++ JsFunctions/JsFunc1.js
@@ -75,10 +75,34 @@ function gret(){
     console.log("hello");
 }
 
-
 function ptrgret(fun,num){
     for(let i = 0; i<num; i++){
         fun()
     }
 }
 ptrgret(gret,4)
+
+
+ 
+
+//higher order function return
+function odev(request){
+     if (request == "even") {
+        return function(n){
+            console.log(n % 2 == 0);
+        }
+       
+     }
+     else if (request == "odd") {
+        return function(n){
+            console.log(n % 2 != 0);
+        }
+    
+     }
+     else{
+        console.log("incorrect request");
+     }
+}
+let request = "odd"
+let func = odev(request)
+func(4)
---
+++ JsFunctions/JsFunc2.js
@@ -0,0 +1,13 @@
+//Methods 
+//object leterls with functions
+const calsulate = {
+    add:function(a,b){
+        return a+b
+    },
+    sub:function(a,b){
+        return a-b;
+    }
+}
+console.log(calsulate.add(4,4));
+console.log(calsulate.sub(4,4));
+ 
