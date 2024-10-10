3cf310116e3823fd127b2f5d5896ef1b7d0d5813
bishlrajbanshi <bishalrajbanshi.in@gmail.com>
2024-10-10 18:44:36 +0545

    day10

--- JsFunctions/JsFunc2.js
+++ JsFunctions/JsFunc2.js
@@ -11,3 +11,29 @@ const calsulate = {
 console.log(calsulate.add(4,4));
 console.log(calsulate.sub(4,4));
  
+
+//this keywords
+//this Keyword  "This" keyword refers to an object that is executing the current piece of code.
+const std  = {
+    name: "bishal",
+    age: 21,
+    eng: 33,
+    math:100,
+    phy:88,
+
+    getAvg(){
+        //we cannot directly access the std key so to access the key we use this keyword
+        console.log(this);
+        
+        let avg = (this.eng + this.math + this.phy) / 3
+        console.log(avg);
+        
+    }
+
+}
+
+
+std.getAvg()
+
+
+
