c42a6d2c4cd139430c0a0d1ebea585b3e5190339
bishlrajbanshi <bishalrajbanshi.in@gmail.com>
2024-10-10 18:53:05 +0545

    day10

---
+++ JsFunctions/JsArrowFun3.js
@@ -0,0 +1,12 @@
+//arrow functions it is used as a value 
+// const function = (arg1,arg2,...)=>{
+    //code
+// }
+
+
+const mul = (a,b)=>{
+   // console.log(a*b);
+   return a*b
+}
+// mul(2,2)
+console.log(mul(2,2));
