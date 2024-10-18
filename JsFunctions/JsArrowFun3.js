7a18ea256afb33460dc24d9457edb3f1fb300a8d
bishlrajbanshi <bishalrajbanshi.in@gmail.com>
2024-10-18 19:07:23 +0545

    day18

--- JsFunctions/JsArrowFun3.js
+++ JsFunctions/JsArrowFun3.js
@@ -32,10 +32,19 @@ console.log("buy");
 //set time interval 
 // time -> excute -> time -> excute
 
-let idx = setInterval(() => {
-    console.log("apna collage");
+// let idx = setInterval(() => {
+//     console.log("apna collage");
     
-}, 2000);
+// }, 2000);
 
-
+// Qs1. Square and sum the array elements using the arrow function and then find the average of the array
  
+
+let nums = [2,4,6,8,10]
+let sqrt = nums.map((nums)=> (nums * nums))
+let sqtsum = sqrt.reduce((acc,el)=>(acc+el))
+let avg = sqtsum /nums.length
+
+console.log(sqrt);
+console.log(sqtsum);
+console.log(avg);
