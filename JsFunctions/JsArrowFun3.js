3445269608b1689a7bb325f72bc48ded90162e39
bishlrajbanshi <bishalrajbanshi.in@gmail.com>
2024-10-19 19:17:55 +0545

    day19

--- JsFunctions/JsArrowFun3.js
+++ JsFunctions/JsArrowFun3.js
@@ -48,3 +48,37 @@ let avg = sqtsum /nums.length
 console.log(sqrt);
 console.log(sqtsum);
 console.log(avg);
+
+
+// Qs2. Create a new array using the map function whose each element is equal to the original element plus 5
+
+let numbers = [2,3,4,5]
+
+console.log(numbers.map((numbers)=> numbers + 5));
+
+
+
+//. Create a new array whose elements are in uppercase of words present in the original array
+
+let personNames = ["bishal","admin","akash"]
+console.log(personNames.map((ele)=> ele.toUpperCase()));
+
+
+
+//Write a function called doubleAndReturnArgs which accepts an array and avariable number of arguments. The function should return a new array with the original array values and all of the additional arguments doubled
+
+const doubleReturnArgs = (arr4,...args0)=>[
+    ...arr4,
+    ...args0.map((el)=>el*2),
+]
+
+console.log(doubleReturnArgs([1,2,3],4,4));
+
+
+//. Write a function called mergeObjects that accepts two objects and returns a new object which contains all the keys and values of the first object and second object
+
+
+const meargobj = (obj1,obj2)=>(
+    {...obj1,...obj2}
+)
+console.log({name:"bishal"},{name:"admin"});
